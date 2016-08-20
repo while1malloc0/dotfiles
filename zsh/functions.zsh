fcfe4bb4a51f28ff7f4ae55363dd06071c11f8c6
John Turner <while1malloc0@gmail.com>
2016-08-20 00:55:59 -0400

    Add watch function to zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -5,3 +5,25 @@ brew-up(){
     brew prune;
     brew doctor;
 }
+
+watch(){
+  CTIME=$(stat -f "%m" scratch);
+  FILE=$1
+  CMD=$2
+  clear
+  echo "Watching $FILE..."
+  while true; do
+    if [ $(stat -f "%m" $FILE) -gt $CTIME ]
+      then
+        clear
+        echo "$FILE changed. Executing $CMD"
+        echo "==========================="
+        CTIME=$(stat -f "%m" $FILE);
+        $CMD;
+        echo "==========================="
+        echo "Watching $FILE..."
+    fi
+    sleep 1;
+  done
+}
+
