4f5849893b2dcd1ed0ec8d49cd8477e1dbef6ce1
John Turner <johnathan.turner@betterment.com>
2018-04-05 13:25:54 -0400

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
 
-# bootstrap_list=( 'zsh' 'tmux' )
-
-# for folder in "${bootstrap_list[@]}"; do
-#   pushd $folder
-#     echo 'Bootstrapping "$folder"'
-#     $PWD/bootstrap.sh
-#   popd
-# done
-
-command -v "python" &> /dev/null || { echo "Please install python"; exit 1; }
-
-command -v "pip" &> /dev/null || { echo "Please install pip"; exit 1; }
-
-command -v "ansible-playbook" &> /dev/null || pip install ansible-playbook
-
-ansible-playbook -i "localhost," -c local bootstrap.yml
+find . -name bootstrap.sh -exec bash -c "$PWD/{}" {} \;
